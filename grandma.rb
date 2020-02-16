0bc3d9d1ab1cf096c4a75267460fdbb704f9c084
Joel Hobgood <jhobgoo@gmail.com>
2020-02-16 01:38:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,9 @@
   def speak_to_grandma(phrase)
     if phrase == 'I LOVE YOU GRANDMA!'
       'I LOVE YOU TOO PUMPKIN!'
-    else
+    elsif
       'HUH?! SPEAK UP, SONNY!'
+    else
+      'NO, NOT SINCE 1938!'
     end
   end
